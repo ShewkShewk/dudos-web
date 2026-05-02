ea927066581753ccbdab4d77d8bac6d1b2e49af5
shuk <javierjg0709@gmail.com>
2026-05-02 12:27:34 -0500

    Add table that shows available tournaments

--- app/tournaments/page.tsx
+++ app/tournaments/page.tsx
@@ -1,3 +1,25 @@
-export default function Tournaments() {
-	return <div>Tournaments</div>
+import { GetTournaments } from "@/app/lib/client";
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+
+export default async function Tournaments() {
+	const tournaments = await GetTournaments()
+	return (
+		<Table>
+			<TableCaption>Tournaments available for import.</TableCaption>
+			<TableHeader>
+				<TableRow>
+					<TableHead>Name</TableHead>
+					<TableHead>Date</TableHead>
+				</TableRow>
+			</TableHeader>
+			<TableBody>
+				{tournaments.map(tournament => (
+					<TableRow key={tournament.id}>
+						<TableCell>{tournament.name}</TableCell>
+						<TableCell>{tournament.date}</TableCell>
+					</TableRow>
+				))}
+			</TableBody>
+		</Table>
+	)
 }

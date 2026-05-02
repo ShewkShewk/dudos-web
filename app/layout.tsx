e65b6b92939f2c395c1a5a058730e3d1c7d087bd
shuk <javierjg0709@gmail.com>
2026-05-02 12:01:37 -0500

    Refactor to enable side bar navigation that populates main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,37 +3,79 @@ import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { auth } from "@/auth";
+import LoginPage from "@/app/login/page";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { Separator } from "@/components/ui/separator";
+import ExampleServerComponent from "@/app/server-component";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList } from "@/components/ui/breadcrumb";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+const inter = Inter({subsets: ['latin'], variable: '--font-sans'});
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "dudOS",
-  description: "An operating system for the Dallas Urban Debate Alliance",
+	title: "dudOS",
+	description: "An operating system for the Dallas Urban Debate Alliance",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+export default async function RootLayout({
+											 children,
+										 }: Readonly<{
+	children: React.ReactNode;
 }>) {
-  return (
-    <html
-      lang="en"
-      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
-    >
-      <body className="min-h-full flex flex-col">
-        <TooltipProvider>{children}</TooltipProvider>
-      </body>
-    </html>
-  );
+	const session = await auth()
+	if (!session) return <LoginPage/>
+	return (
+		<html
+			lang="en"
+			className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
+		>
+		<body className="min-h-full flex flex-col">
+		<TooltipProvider>
+			<SidebarProvider>
+				<AppSidebar user={
+					{
+						name: session.user?.name ?? "User",
+						email: session?.user?.email ?? "user@example.com",
+						avatar: session.user?.image ?? ""
+					}
+				}/>
+				<SidebarInset>
+					<header className="flex h-16 shrink-0 items-center gap-2">
+						<div className="flex items-center gap-2 px-4">
+							<SidebarTrigger className="-ml-1"/>
+							<Separator
+								orientation="vertical"
+								className="mr-2 data-vertical:h-4 data-vertical:self-auto"
+							/>
+							<ExampleServerComponent/>
+							<Breadcrumb>
+								<BreadcrumbList>
+									<BreadcrumbItem className="hidden md:block">
+										<BreadcrumbLink href="#">
+											dudOs
+										</BreadcrumbLink>
+									</BreadcrumbItem>
+								</BreadcrumbList>
+							</Breadcrumb>
+						</div>
+					</header>
+					{children}
+				</SidebarInset>
+			</SidebarProvider>
+			)
+		</TooltipProvider>
+		</body>
+		</html>
+	);
 }

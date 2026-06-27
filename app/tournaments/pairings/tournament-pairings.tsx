eb921c6efdfc5279724b37f452e047b357203fc1
shuk <javierjg0709@gmail.com>
2026-06-27 12:25:57 -0500

    Add button to refresh tournaments within tournament-pairings page

--- app/tournaments/pairings/tournament-pairings.tsx
+++ app/tournaments/pairings/tournament-pairings.tsx
@@ -1,32 +1,28 @@
 import { Tournament } from "@/app/lib/domain";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { TournamentPairings } from "@/app/lib/domain";
 import { EventPairingTable } from "@/app/tournaments/pairings/event-pairings";
+import { useSharedTournaments } from "@/app/tournaments/TournamentsDataProvider";
+import { Button } from "@/components/ui/button";
 
 interface TournamentPairingsProps {
 	tournament: Tournament
 }
 
 export function TournamentPairingsTable({tournament}: TournamentPairingsProps) {
+	const {refreshTournaments, importTournament} = useSharedTournaments()
 	const [tournamentPairings, setTournamentPairings] = useState<TournamentPairings | null>(null)
-	useEffect(() => {
-		const getTournamentPairings = async () => {
-			const response = await fetch(`/api/pairings/${tournament.id}`)
-			if (!response.ok) {
-				throw new Error(`Failed to fetch latest pairings for ${tournament.id}`)
-			}
-			setTournamentPairings(await response.json() as TournamentPairings)
+	const refreshTournamentPairings = useCallback(async () => {
+		const response = await fetch(`/api/pairings/${tournament.id}`)
+		if (!response.ok) {
+			throw new Error(`Failed to fetch latest pairings for ${tournament.id}`)
 		}
-		getTournamentPairings()
-	}, [tournament])
-	if (tournament == null) {
-		return (
-			<div>
-				No tournament selected
-			</div>
-		)
-	}
+		response.json().then(r => setTournamentPairings(r))
+	}, [tournament.id])
 
+	useEffect(() => {
+		refreshTournamentPairings()
+	}, [tournament])
 	if (tournamentPairings == null) {
 		return (
 			<div>
@@ -38,9 +34,22 @@ export function TournamentPairingsTable({tournament}: TournamentPairingsProps) {
 
 	return (
 		<div>
-			<h2 className="text-center bg-blue-400 rounded-md font-bold m-2">
-				Last updated: {tournamentPairings.updateTime}
-			</h2>
+			<div className="flex mt-1 ml-1 mr-1">
+				<h2 className="flex text-center items-center justify-center bg-blue-400 w-9/10 rounded-l-lg font-bold">
+					Last Updated: {tournamentPairings.updateTime}
+				</h2>
+				<Button
+					className="w-1/10 bg-green-300 rounded-none rounded-r-lg border-0 text-black cursor-pointer"
+					onClick={
+						async () => {
+							await importTournament(tournament.id)
+							await refreshTournaments()
+							await refreshTournamentPairings()
+						}}>
+					Refresh
+				</Button>
+			</div>
+
 			<div className="min-w-0 max-w-full columns-1 gap-1 space-y-1 overflow-hidden md:columns-2">
 				{
 					tournamentPairings.eventPairings.map(pairing => (

f93d61e915a272f5d217a5d2dacdc89386a69143
shuk <javierjg0709@gmail.com>
2026-06-27 09:35:10 -0500

    Add header showing update time within tournament pairings

--- app/tournaments/pairings/tournament-pairings.tsx
+++ app/tournaments/pairings/tournament-pairings.tsx
@@ -37,15 +37,21 @@ export function TournamentPairingsTable({tournament}: TournamentPairingsProps) {
 	tournamentPairings.eventPairings.sort((a, b) => a.pairings.length - b.pairings.length)
 
 	return (
-		<div className="min-w-0 max-w-full columns-1 gap-1 space-y-1 overflow-hidden md:columns-2">
-			{
-				tournamentPairings.eventPairings.map(pairing => (
-					<div key={pairing.name}
-					     className="inline-block w-full min-w-0 max-w-full break-inside-avoid overflow-hidden">
-						<EventPairingTable eventPairing={pairing}/>
-					</div>
-				))
-			}
+		<div>
+			<h2 className="text-center bg-blue-400 rounded-md font-bold m-2">
+				Last updated: {tournamentPairings.updateTime}
+			</h2>
+			<div className="min-w-0 max-w-full columns-1 gap-1 space-y-1 overflow-hidden md:columns-2">
+				{
+					tournamentPairings.eventPairings.map(pairing => (
+						<div key={pairing.name}
+						     className="inline-block w-full min-w-0 max-w-full break-inside-avoid overflow-hidden">
+							<EventPairingTable eventPairing={pairing}/>
+						</div>
+					))
+				}
+			</div>
 		</div>
+
 	)
 }
